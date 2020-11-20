b83bb2fa76aca3e262e92f25ab530be26dab3981
Bruno Martins <bgcngm@gmail.com>
2020-11-20 20:12:15 +0000

    pdx206: Remove deprecated telephony flag
    The custom jar is dead for good in Android 11.
    
    Change-Id: I7f0f7afd186b00f01183bf4479092d03897e074a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -190,9 +190,6 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Security Patch Level
 VENDOR_SECURITY_PATCH := 2022-10-01
 
-# Telephony
-TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
-
 # SEPolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
