3693d1c993579c0f539446fb10fc12e2dfef3544
Alexander Koskovich <akoskovich@pm.me>
2022-04-13 16:25:29 -0700

    pdx206: Drop PRODUCT_BUILD_SUPER_PARTITION
     * Super image is not generated by a default 'm' build,
       but we at least want it for dist builds.
    
    Change-Id: Ia12a5314632509fcd309d51ddf757efdc37499c3

--- device.mk
+++ device.mk
@@ -264,7 +264,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
 # Dynamic Partitions
-PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # fastbootd
