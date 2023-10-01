7e9ba4a7d678e142d82ca6e151812b871438df73
ad1tyas1ngh <adityasingh1768@gmail.com>
2023-10-01 07:45:03 +0530

    pdx206: Remove duplicate ueventd.qcom.rc
     * This was not being copied
    
    Signed-off-by: ad1tyas1ngh <adityasingh1768@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -174,7 +174,7 @@ LOCAL_MODULE       := ueventd.qcom.rc
 LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := ueventd.rc
+LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
 
