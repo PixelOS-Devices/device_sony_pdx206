b25bba1eb76498b94042f49cb57b6978054303ce
ad1tyas1ngh <adityasingh1768@gmail.com>
2023-11-07 02:03:32 +0530

    pdx206: Patchelf blobs to use libprotobuf-cpp-full-3.9.1
     * libprotobuf-cpp-lite-3.9.1 is now depreciated
    
    Signed-off-by: ad1tyas1ngh <adityasingh1768@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -64,6 +64,9 @@ function blob_fixup() {
     vendor/lib64/vendor.somc.camera* | vendor/bin/hw/vendor.somc.hardware.camera.*)
         "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
         ;;
+    vendor/bin/sensors.qti | vendor/lib*/libwvhidl.so | vendor/lib*/mediadrm/libwvdrmengine.so | vendor/lib*/sensors.ssc.so | vendor/lib64/libsensorcal.so | vendor/lib64/libsnsapi.so | vendor/lib64/libsnsdiaglog.so | vendor/lib64/libssc.so)
+        "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
+        ;;
     esac
 }
 
