353122605807792860ee87a059ed99a0d80b2c6b
Michael Bestas <mkbestas@lineageos.org>
2023-12-04 02:26:20 +0530

    pdx206: Don't build unused QTI wifi libs
    Change-Id: I2bd14d587ff1063667f09cb1f284ad8aa58ef0ec

--- device.mk
+++ device.mk
@@ -529,8 +529,6 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
-    vendor.qti.hardware.wifi.hostapd@1.2.vendor \
-    vendor.qti.hardware.wifi.supplicant@2.1.vendor \
     wpa_supplicant \
     wpa_supplicant.conf
 
